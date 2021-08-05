db3b94574690457e6f8781aa361ce1cb01e684d3
Johnny Chen <johnnychen94@hotmail.com>
2021-08-05 18:48:50 +0800

    Update src/linalg.jl

--- src/linalg.jl
+++ src/linalg.jl
@@ -1,5 +1,13 @@
 # declare struct 
-using SparseArrays: DenseMatrixUnion, AdjOrTransDenseMatrix, DenseInputVector, DenseInputVecOrMat
+if VERSION >= v"1.6"
+    # this union alias is added in https://github.com/JuliaLang/julia/pull/39557
+    using SparseArrays: DenseMatrixUnion, AdjOrTransDenseMatrix, DenseInputVector, DenseInputVecOrMat 
+else
+    const DenseMatrixUnion = Union{StridedMatrix, LowerTriangular, UnitLowerTriangular, UpperTriangular, UnitUpperTriangular, BitMatrix}
+    const AdjOrTransDenseMatrix = Union{DenseMatrixUnion,Adjoint{<:Any,<:DenseMatrixUnion},Transpose{<:Any,<:DenseMatrixUnion}}
+    const DenseInputVector = Union{StridedVector, BitVector}
+    const DenseInputVecOrMat = Union{AdjOrTransDenseMatrix, DenseInputVector}
+end
 
 
 @i function imul!(C::StridedVecOrMat, A::AbstractSparseMatrix, B::DenseInputVecOrMat, α::Number, β::Number) 
