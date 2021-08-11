769be5827ad328a77b9000ce04db687d7ce76cb1
Johnny Chen <johnnychen94@hotmail.com>
2021-08-11 11:39:31 +0800

    add rrule for `*(::Sparse, ::Dense)` (#18)
    * add rrule for `*(::Sparse, ::Dense)`
    
    * Update src/compat.jl
    
    Co-authored-by: Leo <cacate0129@gmail.com>
    
    Co-authored-by: Jie Li <76780849+jieli-matrix@users.noreply.github.com>
    Co-authored-by: Leo <cacate0129@gmail.com>

--- src/NiSparseArrays.jl
+++ src/NiSparseArrays.jl
@@ -3,6 +3,10 @@ module NiSparseArrays
 using LinearAlgebra, SparseArrays
 using NiLang
 using NiLang.AD 
+using ChainRulesCore
 
+include("compat.jl")
 include("linalg.jl")
+include("chainrules.jl")
+
 end
