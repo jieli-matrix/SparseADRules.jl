35d11f81cc1143a5863d7ce4b184d75aaf47123c
jieli-matrix <li_j20@fudan.edu.cn>
2021-08-16 19:52:41 +0800

    add more rrules

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,8 +1,43 @@
-function ChainRulesCore.rrule(::typeof(*), A::AbstractSparseMatrix, B::DenseInputVecOrMat)
+function ChainRulesCore.rrule(
+    ::typeof(*), A::AbstractSparseMatrix{T}, B::DenseInputVecOrMat
+) where T 
     C = A * B
     function mul_pullback(C̄)
         _, i_A, i_B, _, _ = (~imul!)(AD.GVar(C, C̄), AD.GVar(A), AD.GVar(B), AD.GVar(1.), AD.GVar(1.))
         return ChainRulesCore.NoTangent(), AD.grad(i_A), AD.grad(i_B)
+    end
+    return C, mul_pullback
+end
+
+function ChainRulesCore.rrule(
+    ::typeof(*), xA::Adjoint{T, <:AbstractSparseMatrix}, B::DenseInputVecOrMat
+) where T
+    C = xA * B
+    function mul_pullback(C̄)
+        _, i_xA, i_B, _, _ = (~imul!)(AD.GVar(C, C̄), AD.GVar(xA), AD.GVar(B), AD.GVar(1.), AD.GVar(1.))
+        return ChainRulesCore.NoTangent(), AD.grad(i_xA), AD.grad(i_B)
+    end
+    return C, mul_pullback
+end
+
+function ChainRulesCore.rrule(
+    ::typeof(*), X::DenseInputVecOrMat,A::AbstractSparseMatrix{T}
+) where T 
+    C = X * A
+    function mul_pullback(C̄)
+        _, i_X, i_A, _, _ = (~imul!)(AD.GVar(C, C̄), AD.GVar(X), AD.GVar(A), AD.GVar(1.), AD.GVar(1.))
+        return ChainRulesCore.NoTangent(), AD.grad(i_X), AD.grad(i_A)
+    end
+    return C, mul_pullback
+end
+
+function ChainRulesCore.rrule(
+    ::typeof(*), X::Adjoint{T1, <:DenseInputVecOrMat}, A::AbstractSparseMatrix{T2}
+) where {T1, T2} 
+    C = X * A
+    function mul_pullback(C̄)
+        _, i_X, i_A, _, _ = (~imul!)(AD.GVar(C, C̄), AD.GVar(X), AD.GVar(A), AD.GVar(1.), AD.GVar(1.))
+        return ChainRulesCore.NoTangent(), AD.grad(i_X), AD.grad(i_A)
     end
     return C, mul_pullback
 end
--- test/chainrules.jl
+++ test/chainrules.jl
@@ -1,15 +1,35 @@
 @testset "test rrule" begin
-    @testset "mul" begin
+    @testset "matrix multiplication" begin
         # sparse * vector
-        A = sprand(5, 3, 0.5)
-        B = rand(3)
-        test_rrule(*, A ⊢ sprand_tangent(A), B; check_thunked_output_tangent=false)
-        # test_rrule(*, A ⊢ sprand_tangent(A), B) # FIXME: Thunk doesn't yet supported.
+        #for T in (Float64)
+            A = sprand(5, 3, 0.5)
+            B = rand(3)
+            test_rrule(*, A ⊢ sprand_tangent(A), B; check_thunked_output_tangent=false)
+            # test_rrule(*, A ⊢ sprand_tangent(A), B) # FIXME: Thunk doesn't yet supported.
 
-        # sparse * dense matrix
-        A = sprand(5, 3, 0.5)
-        B = rand(3, 2)
-        test_rrule(*, A ⊢ sprand_tangent(A), B; check_thunked_output_tangent=false)
-        # test_rrule(*, A ⊢ sprand_tangent(A), B) # FIXME: Thunk doesn't yet supported.
+            # sparse * dense matrix
+            A = sprand(5, 3, 0.5)
+            B = rand(3, 2)
+            test_rrule(*, A ⊢ sprand_tangent(A), B; check_thunked_output_tangent=false)
+            # test_rrule(*, A ⊢ sprand_tangent(A), B) # FIXME: Thunk doesn't yet supported.
+        #end 
+    end
+
+    @testset "dense matrix-sparse matrix multiplication" begin
+        # dense matrix * sparse matrix
+        #for T in (Float64)
+            B = rand(10, 10)
+            A = sprand(10, 5, 0.5)
+            test_rrule(*, B, A ⊢ sprand_tangent(A); check_thunked_output_tangent=false)
+        #end 
+    end
+
+    @testset "adjoint dense matrix-sparse matrix multiplication" begin
+    #     # adjoint dense matrix * sparse matrix
+    #     for T in (Float64)
+            B = rand(T, 10, 5)
+            A = sprand(T, 10, 5, 0.5)
+            test_rrule(*, B', A ⊢ sprand_tangent(A); check_thunked_output_tangent=false)
+    #     end 
     end
 end
