cd23dd72606ceab7f4a3884736d36c772cc24539
jieli-matrix <li_j20@fudan.edu.cn>
2021-09-14 13:27:34 +0800

    improve code converage

--- test/lowranksvd.jl
+++ test/lowranksvd.jl
@@ -6,15 +6,29 @@ using NiSparseArrays:get_approximate_basis, low_rank_svd
             for i = 1:5
                 A = sprand(n, m, 0.1)
                 r = rank(A) # sparse matrix not always low rank 
-                @testset "Q basis" begin
+                @testset "Q basis for A" begin
                     Q = get_approximate_basis(A, r)
                     @test norm(A - Q*Q'*A) < approx_rtol*norm(A)
                 end
                 
-                @testset "svd test" begin
+                @testset "svd test for A" begin
                     U, S, Vt = low_rank_svd(A, r)
                     A_approx = U * Diagonal(S) * Vt
                     @test norm(A - A_approx) < approx_rtol*norm(A)
+                end
+
+                M = sprand(n, m, 0.1)
+                A_m = A - M
+                r_m = rank(A-M)
+                @testset "Q basis for A - M" begin
+                    Q = get_approximate_basis(A, r_m, 2, M)
+                    @test norm(A_m - Q*Q'*A_m) < approx_rtol*norm(A_m)
+                end
+                
+                @testset "svd test for A - M" begin
+                    U, S, Vt = low_rank_svd(A, r_m, 2, M)
+                    A_m_approx = U * Diagonal(S) * Vt
+                    @test norm(A_m - A_m_approx) < approx_rtol*norm(A_m)
                 end
             end    
         end
